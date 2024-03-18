438e3edfba6c14049d8ff09d465c87a63b2b2475
Akshat Rajvanshi <akshatrajvanshi14@gmail.com>
2024-03-18 13:16:50 +0530

    New & Create Added

--- app.js
+++ app.js
@@ -15,16 +15,32 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-    -
-    app.get('/', (req, res) => {
-        res.render('home');
-    })
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({ title: "My Backyard", description: "Cheap Camping" });
-    await camp.save();
-    res.send(camp)
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
 })
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+});
 
 app.listen(3000, () => {
     console.log("Serving on port 3000.")
